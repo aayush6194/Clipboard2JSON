57f7ba8b6ea7c4b1f30dc96dc4dc7a73363d2a6d
lawrencek0 <l.khadka@outlook.com>
2019-05-14 14:27:36 -0500

    separate linux and windows functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+#[cfg(target_os = "linux")]
 mod app;
 
+#[cfg(target_os = "linux")]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let dpy = app::App::new()?;
     dpy.get_targets();
 
     Ok(())
+}
+
+#[cfg(target_os = "windows")]
+fn main() {
+    println!("WIP for windows");
 }
