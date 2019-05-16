baaa07ee76f4e012dcd0503487a7a1ee5f09b7c5
lawrencek0 <l.khadka@outlook.com>
2019-05-15 21:05:25 -0500

    add function to get list of all formats and update method to get unicode data

--- src/winapi.rs
+++ src/winapi.rs
@@ -1,24 +1,59 @@
+use std::collections::HashSet;
 use std::ffi::OsStr;
 use std::iter::once;
 use std::os::windows::ffi::OsStrExt;
 use std::ptr::null_mut;
+use winapi::ctypes::wchar_t;
 use winapi::shared::minwindef::{LPARAM, LRESULT, WPARAM};
 use winapi::shared::windef::{HWND, POINT};
 use winapi::um::libloaderapi::GetModuleHandleW;
+use winapi::um::winbase::{GlobalLock, GlobalSize, GlobalUnlock};
 use winapi::um::winuser::{
     AddClipboardFormatListener, CloseClipboard, CreateWindowExW, DefWindowProcW, DispatchMessageW,
-    GetClipboardData, GetMessageW, IsClipboardFormatAvailable, OpenClipboard, PostQuitMessage,
-    RegisterClassW, RemoveClipboardFormatListener, TranslateMessage, CF_TEXT, CS_OWNDC,
-    CW_USEDEFAULT, HWND_MESSAGE, MSG, WM_CLIPBOARDUPDATE, WM_DESTROY, WNDCLASSW, WS_MINIMIZE,
+    EnumClipboardFormats, GetClipboardData, GetMessageW, IsClipboardFormatAvailable, OpenClipboard,
+    PostQuitMessage, RegisterClassW, RegisterClipboardFormatW, RemoveClipboardFormatListener,
+    TranslateMessage, CF_UNICODETEXT, CS_OWNDC, CW_USEDEFAULT, HWND_MESSAGE, MSG,
+    WM_CLIPBOARDUPDATE, WM_DESTROY, WNDCLASSW, WS_MINIMIZE,
 };
 
-pub fn get_clipboard() {
+fn get_formats() -> HashSet<u32> {
+    let mut v = HashSet::new();
     unsafe {
         if OpenClipboard(null_mut()) != 0 {
-            if IsClipboardFormatAvailable(CF_TEXT) != 0 {
-                let a = GetClipboardData(CF_TEXT) as *mut i8;
-                let b = std::ffi::CString::from_raw(a);
-                println!("{:?}", b);
+            let mut format = EnumClipboardFormats(0);
+            loop {
+                if format == 0 {
+                    break;
+                }
+                v.insert(format);
+                format = EnumClipboardFormats(format);
+            }
+        }
+    }
+    v
+}
+
+pub fn get_clipboard() {
+    let formats = get_formats();
+    unsafe {
+        if OpenClipboard(null_mut()) != 0 {
+            // check for CF_CLIPBOARD
+            let html_wide: Vec<u16> = OsStr::new("HTML Format")
+                .encode_wide()
+                .chain(once(0))
+                .collect();
+            let cf_html = RegisterClipboardFormatW(html_wide.as_ptr());
+
+            if formats.contains(&cf_html) {
+
+            } else if IsClipboardFormatAvailable(CF_UNICODETEXT) != 0 {
+                let data = GetClipboardData(CF_UNICODETEXT);
+                let data = GlobalLock(data);
+                let len = GlobalSize(data) / std::mem::size_of::<wchar_t>() - 1;
+                let v = Vec::from_raw_parts(data as *mut u16, len, len);
+                let str = String::from_utf16(&v);
+                println!("{:?}", str);
+                GlobalUnlock(data);
             }
             CloseClipboard();
         }
