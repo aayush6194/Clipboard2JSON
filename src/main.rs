4c00e6b93508cbda3a2b6eaf9070a2b5d19cc876
lawrencek0 <l.khadka@outlook.com>
2019-05-18 20:33:10 -0500

    simplify main function for watching clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
-mod utils;
+use clipboard_rs::{Clipboard, ClipboardFunctions};
 
-#[cfg(target_os = "linux")]
-mod x11_clipboard;
-
-#[cfg(target_os = "linux")]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let dpy = x11_clipboard::Clipboard::new()?;
-    dpy.watch_clipboard()?;
+    let dpy = Clipboard::new()?;
+    dpy.watch_clipboard();
     Ok(())
-}
-
-#[cfg(target_os = "windows")]
-mod winapi_clipboard;
-
-#[cfg(target_os = "windows")]
-fn main() {
-    winapi_clipboard::monitor_clipboard();
 }
