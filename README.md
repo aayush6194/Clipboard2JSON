0f818c145cc135d10cd5ae26d18234a233fd409c
lawrencek0 <l.khadka@outlook.com>
2019-05-18 22:29:19 -0500

    move intro to readme file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Clipboard2JSON
+
+Clipboard2JSON is a tool that watches the window's clipboard and writes the
+contents to a JSON file when the clipboard selection changes. It abstracts
+over the WinAPI and the X11 library to provide a common interface for working
+with the clipboard.
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,3 @@
-//! # Clipboard2JSON
-//! Clipboard2JSON is a tool that watches the window's clipboard and writes the 
-//! contents to a JSON file when the clipboard selection changes. It abstracts 
-//! over the WinAPI and the X11 library to provide a common interface for working
-//! with the clipboard.
 mod common;
 pub use common::ClipboardFunctions;
 
