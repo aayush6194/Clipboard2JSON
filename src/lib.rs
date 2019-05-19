ff72ec5969f4c8f56228e3b878a4d3b49535cb9e
lawrencek0 <l.khadka@outlook.com>
2019-05-18 20:32:54 -0500

    use types to abstract over different clipboard implementations

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+mod utils;
+mod common;
+pub use common::ClipboardFunctions;
+
+#[cfg(target_os = "linux")]
+pub mod x11_clipboard;
+
+#[cfg(target_os = "windows")]
+pub mod winapi_clipboard;
+
+#[cfg(target_os = "linux")]
+pub type Clipboard = x11_clipboard::Clipboard;
