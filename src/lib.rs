6cb45609ca23ffa8fcc361ceb5e781741d0171e9
lawrencek0 <l.khadka@outlook.com>
2019-05-18 22:28:15 -0500

    add intro comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+//! # Clipboard2JSON
+//! Clipboard2JSON is a tool that watches the window's clipboard and writes the 
+//! contents to a JSON file when the clipboard selection changes. It abstracts 
+//! over the WinAPI and the X11 library to provide a common interface for working
+//! with the clipboard.
 mod common;
-pub mod utils;
 pub use common::ClipboardFunctions;
 
 #[cfg(target_os = "linux")]
