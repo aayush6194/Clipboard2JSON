c68f9516a5dc56afc477ac9c0fd7f6cf82652306
lawrencek0 <l.khadka@outlook.com>
2019-05-19 20:58:12 -0500

    add platform specific types

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod common;
-pub use common::ClipboardFunctions;
+pub use common::{ClipboardFunctions, ClipboardSink};
 
 #[cfg(target_os = "linux")]
 pub mod x11_clipboard;
@@ -9,4 +9,12 @@ pub mod winapi_clipboard;
 
 #[cfg(target_os = "linux")]
 pub type Clipboard = x11_clipboard::Clipboard;
+
+#[cfg(target_os = "windows")]
+pub type Clipboard = winapi_clipboard::Window;
+
+#[cfg(target_os = "linux")]
 pub type ClipboardData = x11_clipboard::ClipboardData;
+
+#[cfg(target_os = "windows")]
+pub type ClipboardData = winapi_clipboard::ClipboardData;
