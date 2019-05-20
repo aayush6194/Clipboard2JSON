9167dea15f5b82fb822a7d6c3cc427277a84ed45
lawrencek0 <l.khadka@outlook.com>
2019-05-19 20:58:04 -0500

    update traits to make the interface more cross-platform

--- src/common.rs
+++ src/common.rs
@@ -2,17 +2,18 @@ use crate::ClipboardData;
 use failure::Error;
 
 /// Defines common traits for the clipboard so that it's easier to abstract over
-/// the underlying libraries. 
+/// the underlying libraries.
 // @TODO: Add more functions when workng on WINAPI
 pub trait ClipboardFunctions: Sized {
     /// Creates a new `Clipboard` with a pointer to the hidden window
     // @TODO: Better error handling?
     fn new() -> Result<Self, Error>;
     /// Fetches the data stored in the clipboard as a text-based format
-    fn get_clipboard(&self) -> Result<ClipboardData, Error>; 
+    fn get_clipboard(&self) -> Result<ClipboardData, Error>;
     /// Watches over the clipboard and passes the changed data to the callback
     fn watch_clipboard(&self, callback: &ClipboardSink);
 }
 
 /// Takes the clipboard data and writes it to a source
-pub type ClipboardSink = Fn(ClipboardData) -> Result<(), Error>;
+#[derive(Clone)]
+pub struct ClipboardSink(pub fn(ClipboardData) -> Result<(), Error>);
