bc7dc7f502bb6aaa380a2e11a940def57ce7e74b
lawrencek0 <l.khadka@outlook.com>
2019-05-19 20:58:22 -0500

    use the updated traits

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod utils;
 
-use clipboard2json::{Clipboard, ClipboardFunctions};
+use clipboard2json::{Clipboard, ClipboardFunctions, ClipboardSink};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let dpy = Clipboard::new()?;
-    dpy.watch_clipboard(&utils::save_clipboard_to_file);
+    dpy.watch_clipboard(&ClipboardSink(utils::save_clipboard_to_file));
     Ok(())
 }
