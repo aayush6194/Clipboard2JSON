13152c823ea018354277b2a8d16cd09a410d7d5a
lawrencek0 <l.khadka@outlook.com>
2019-05-21 11:07:33 -0500

    add instructions to use the bin file

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod utils;
 use clipboard2json::{Clipboard, ClipboardFunctions, ClipboardSink};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Welcome to Clipboard2JSON");
     let dpy = Clipboard::new()?;
+    println!("Try copying some text and it should show up in a clipboard.json file in your folder");
     dpy.watch_clipboard(&ClipboardSink(utils::save_clipboard_to_file));
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use failure::Error;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
+use std::fmt::Debug;
 use std::fs::{File, OpenOptions};
 use std::io::BufReader;
 
@@ -8,8 +9,9 @@ use std::io::BufReader;
 /// overwrites the JSON file.
 pub fn save_clipboard_to_file<T>(data: T) -> Result<(), Error>
 where
-    T: DeserializeOwned + Serialize,
+    T: DeserializeOwned + Serialize + Debug,
 {
+    println!("Clipboard change detected!");
     let mut file = OpenOptions::new()
         .read(true)
         .write(true)
@@ -17,10 +19,12 @@ where
         .open("clipboard.json")?;
     let reader = BufReader::new(&mut file);
     let mut stored_data: Vec<T> = serde_json::from_reader(reader).unwrap_or_default();
+    println!("Writing {:#?} to file...", data);
     stored_data.push(data);
     drop(file); // closes the file so we can overwrite it
     let file = File::create("clipboard.json")?;
     serde_json::to_writer(file, &stored_data)?;
+    println!("Successfuly wrote to clipboard.json");
     Ok(())
 }
 
